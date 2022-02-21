135fdc324d39255eda812d344475e7a53f9de3ce
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2022-02-21 14:50:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from microbit import *
+import neopixel
+import utime
 
+class MaqMicroBit
+
+  def __init__(self):
+    i2c.init()
+    self.np = neopixel.NeoPixel(pin15, 4)
+    pin1.write_digital(0)
